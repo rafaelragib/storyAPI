6222d6a8b348ca0f791f12e58181985050fc449a
Ragib <ragib.un.nur@gmail.com>
2021-06-12 14:12:00 +0600

    post story added

--- controller/storyController.ts
+++ controller/storyController.ts
@@ -1,54 +1,70 @@
-export const storyController = (story:any) =>
-{
-    async function getStory(req:any,res:any) {
-       
-        try
-        {
-            
-            const stories= await story.find({});
-            const storyJSON= stories.map((ele:any) =>{
-                ele=ele.toJSON();
-                ele.links={};
-                ele.links.self=`http://localhost:3000/story/${ele._id}`;
+//var bodyParser = require('body-parser');
+
+export const storyController = (story: any) => {
+    async function getStory(req: any, res: any) {
+
+        try {
+
+            const stories = await story.find({});
+            const storyJSON = stories.map((ele: any) => {
+                ele = ele.toJSON();
+                ele.links = {};
+                ele.links.self = `http://localhost:3000/story/${ele._id}`;
                 return ele;
             })
             res.status(200);
             return res.send(storyJSON);
         }
-        catch(error)
-        {
+        catch (error) {
             res.status(500);
             console.log(error);
             return res.send(error);
         }
-       
+
     }
 
-    async function getStoryById(req:any,res:any) {
-       
-        try
-        {
-            const id=req.params.id;
-            const stories= await story.find({'_id':id});
-            if(stories.length<=0){
-                res.status(404);
+    async function getStoryById(req: any, res: any) {
+
+        try {
+            const id = req.params.id;
+            const stories = await story.find({ '_id': id });
+            if (stories.length <= 0) {
+                res.status(204);
                 return res.send("not found");
             }
-            else{
+            else {
                 res.status(200);
                 return res.send(stories);
             }
-            
+
         }
-        catch(error)
-        {
+        catch (error) {
             res.status(500);
             console.log(error);
             return res.send(error);
         }
-       
+
     }
 
+    async function createStory(req: any, res: any) {
+        try {
+            const newStory = await story.create({
+                'title': req.body.title,
+                'body': req.body.body
+            });
+            if (newStory) {
+                res.status(201);
+                return res.send(newStory);
+            }
+            res.status(500);
+            return res.send("Internal Problem");
+        }
+        catch (error) {
+            res.status(500);
+            return res.send("Bad Request");
+        }
+        //return res.send(req.body);
+    }
 
-    return {getStory,getStoryById}
+    return { getStory, getStoryById, createStory }
 }
--- routes/router.ts
+++ routes/router.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 import {storyController} from '../controller/storyController';
 import {story} from '../models/storyModel';
-var bodyParser = require('body-parser')
+//var bodyParser = require('body-parser')
 
 export const router=express();
-var jsonParser = bodyParser.json()
+//var jsonParser = bodyParser.json()
 const stories =storyController(story);
-
+router.use(express.json());
 router.get('/',(req,res)=>
 {
        
@@ -14,14 +14,6 @@ router.get('/',(req,res)=>
 })
 router.get('/story',stories.getStory);
 router.get('/story/:id',stories.getStoryById);
-router.post('/story',jsonParser,(req,res)=>{
-    // const temp={
-    //     title: req.body.title,
-    //     body:req.body.body
-    // }
-    console.log(req.body);
-    res.send();
-
-});
+router.post('/story',stories.createStory);
 
  
